f5d20162cc45a7e5ac102e5162ac9e0613fb7e93
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-04 15:29:24 -0300

    style(git): creating a theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import { MoonIcon } from "@heroicons/react/24/solid";
+import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
 const Header = () => {
   return (
     <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-      <span className="text-gray-100">Olá, Usuário</span>
+      <span className="text-gray-100">Olá, Kayo Ennrique</span>
       <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
-      <MoonIcon className="h-8 text-gray-100 hidden sm:block cursor-pointer" />
+      <ToggleTheme />
     </div>
   );
 };
