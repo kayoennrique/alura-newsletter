305131996a615390452148f382e3f04e7dde52c0
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-04 15:33:17 -0300

    style(git): night mode
    Making night mode sync with the user's system mode

--- src/components/ToggleTheme/ToggleTheme.jsx
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -1,8 +1,17 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useEffect } from "react";
 
 const ToggleTheme = () => {
+  const systemPreference = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  const pageClasses = document.documentElement.classList;
+
+  useEffect(() => {
+    systemPreference && pageClasses.add("dark");
+  });
   const toggle = () => {
-    document.documentElement.classList.toggle("dark");
+    pageClasses.toggle("dark");
   };
   return (
     <div className="hidden sm:block">
