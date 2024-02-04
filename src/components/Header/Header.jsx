53f7422314cc41de182f3575e7fdc37e02c563f5
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-04 15:37:03 -0300

    style(git): working with icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
 const Header = () => {
   return (
-    <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+    <div className="flex h-20 bg-alura-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
       <span className="text-gray-100">Olá, Kayo Ennrique</span>
       <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
       <ToggleTheme />
