fbc88182f746e23c1fce599d9c252e6b5abf72b8
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-04 15:49:30 -0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
 const Header = () => {
   return (
-    <div className="flex h-20 bg-alura-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-      <span className="text-gray-100">Olá, Kayo Ennrique</span>
-      <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
+    <div className="p-5 w-full sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
+      <span className="text-gray-100">Olá, Usuário</span>
+      <h1 className="text-gray-100 text-2xl sm:text-4xl">Alura Newsletter</h1>
       <ToggleTheme />
     </div>
   );
--- src/components/ToggleTheme/ToggleTheme.jsx
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -1,27 +1,31 @@
-import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
 
 const ToggleTheme = () => {
-  const systemPreference = window.matchMedia(
+  const pageRef = document.documentElement.classList;
+  const systemThemePreference = window.matchMedia(
     "(prefers-color-scheme: dark)"
   ).matches;
-  const pageClasses = document.documentElement.classList;
+  const [dark, setDark] = useState(systemThemePreference);
 
   useEffect(() => {
-    systemPreference && pageClasses.add("dark");
-  });
-  const toggle = () => {
-    pageClasses.toggle("dark");
+    dark && pageRef.add("dark");
+  }, []);
+
+  const toggleTheme = () => {
+    pageRef.toggle("dark");
+    setDark(!dark);
   };
+
   return (
     <div className="hidden sm:block">
-      <MoonIcon
-        className="h-8 text-gray-100 block dark:hidden cursor-pointer"
-        onClick={toggle}
-      />
       <SunIcon
-        className="h-8 text-gray-100 hidden dark:block cursor-pointer"
-        onClick={toggle}
+        className="w-7 h-7 text-gray-100 cursor-pointer hidden dark:block"
+        onClick={toggleTheme}
+      />
+      <MoonIcon
+        className="w-7 h-7 text-gray-100 cursor-pointer block dark:hidden"
+        onClick={toggleTheme}
       />
     </div>
   );
