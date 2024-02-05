cc8368119d8649a4087f8399a8a4a8a490b3901c
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-05 20:45:59 -0300

    feat(git): adding the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Header from "./components/Header/Header";
 import ArticleList from "./components/ArticleList/ArticleList";
 
+import { useState } from "react";
+import Form from "./components/Form/Form";
+
 const App = () => {
+  const [user, setUser] = useState();
+  const hasUser = Boolean(user);
+
   return (
-    <div className="w-screen h-screen sm:px-5 flex flex-col justify-start items-center">
-      <Header />
-      <ArticleList />
-    </div>
+    <>
+      <Header user={user} />
+      {hasUser && <ArticleList />}
+      {hasUser || <Form onSubmit={setUser} />}
+    </>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
-const Header = () => {
+const Header = ({ user }) => {
   return (
     <div className="p-5 w-full sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
-      <span className="text-gray-100">Olá, Usuário</span>
+      <span className="text-gray-100">Olá, {user?.name || "Usuário"}</span>
       <h1 className="text-gray-100 text-2xl sm:text-4xl">Alura Newsletter</h1>
       <ToggleTheme />
     </div>
