664e84bef4b8ced3ef833bdfb0c5ed147c2acbcf
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-06 02:07:40 -0300

    style(yarn): using plugins

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
+
 import Header from "./components/Header/Header";
 import ArticleList from "./components/ArticleList/ArticleList";
-
-import { useState } from "react";
 import Form from "./components/Form/Form";
 
 const App = () => {
   const [user, setUser] = useState();
-  const hasUser = Boolean(user);
+
+  const submitForm = (user) => {
+    setUser(user);
+  };
 
   return (
-    <div className="h-screen">
-      <Header user={user} />
-      {hasUser && <ArticleList />}
-      {hasUser || <Form onSubmit={setUser} />}
+    <div className="w-screen h-screen sm:px-5 flex flex-col justify-start items-center">
+      <Header user={user?.name} />
+      {!!user && <ArticleList />}
+      {!!user || <Form onSubmit={submitForm} />}
     </div>
   );
 };
