983192df04a766576fa790634aadc11f2532bc28
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-06 01:03:21 -0300

    style(git): directives and animations in the form
    n

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,3 +1,5 @@
+import Input from "../Input/Input";
+
 const Form = ({ onSubmit }) => {
   const safeSubmit = (event) => {
     event.preventDefault();
@@ -10,13 +12,13 @@ const Form = ({ onSubmit }) => {
   return (
     <form
       onSubmit={safeSubmit}
-      className="h-full flex flex-col items-center justify-center gap-10 -mt-20"
+      className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5"
     >
-      <input type="text" placeholder="Insira seu nome..." />
-      <input type="email" placeholder="Insira seu e-mail..." />
+      <Input type="text" required placeholder="Insira seu nome..." />
+      <Input type="email" required placeholder="Insira seu e-mail..." />
       <button
         type="submit"
-        className="py-1 px-5 bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase"
+        className="py-1 px-5 w-full max-w-lg bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none"
       >
         Seguir
       </button>
